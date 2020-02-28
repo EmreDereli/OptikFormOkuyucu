7c1b9a5446e312db89d8c0627f8387d20901cbb0
Emre <emre@bigoen.com>
2020-02-28 22:44:57 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
